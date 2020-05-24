12f78446474fa77f896d8aa2bc11e7079554fdd0
panterx <panterx.95@gmail.com>
2020-05-23 21:46:59 -0300

    Clase tabla agregada

---
+++ js/tabla.js
@@ -0,0 +1,16 @@
+class Tabla {
+    constructor(puntos) {
+        this.puntos = puntos; // 15p o 30p
+        this.jugadores = [];
+    };
+
+    dibujarJugadoresEnLaTabla() {
+        this.jugadores.forEach(jugador => {
+            // dibujar jugadores en la tabla
+        });
+    };
+
+    actualizarPuntos() {
+        // agregar puntos a la tabla
+    };
+};
