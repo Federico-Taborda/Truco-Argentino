c98c50bcc9fa48da30f08dbfaeda470e03093d2d
panterx <panterx.95@gmail.com>
2020-05-23 21:47:15 -0300

    Clase partida agregada

---
+++ js/partida.js
@@ -0,0 +1,33 @@
+class Partida {
+    constructor(jugadores, mazo, puntos, tabla) {
+        this.jugadores = jugadores; // Array con todos los jugadores
+        this.mazo = mazo;
+        this.puntos = puntos;
+        this.tabla = tabla;
+    };
+
+    repartirOtroMazo() {
+        return this.mazo.nuevoMazo();
+    };
+
+    sumarPuntosAlJugador(jugador, puntos) {
+        return jugador.puntos += puntos;
+    };
+
+    actualizarPuntosJugador(nombre, puntos) {
+        this.tabla.jugadores.forEach(jugador => {
+            if(jugador.nombre == "nombre") {
+                jugador.puntos = puntos;
+            };
+        });
+    };
+
+    agregarJugadoresTabla() {
+        this.jugadores.forEach(jugador => {
+            this.tabla.jugadores.push({
+                nombre: jugador.nombre,
+                puntos: jugador.puntos
+            });
+        });
+    };
+};
