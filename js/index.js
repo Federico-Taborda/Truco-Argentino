0f24801062aaf79c8e4fb25ed408164351442544
panterx <panterx.95@gmail.com>
2020-05-25 20:39:13 -0300

    Listeners de botones agregados

--- js/index.js
+++ js/index.js
@@ -16,9 +16,72 @@ const partida = new Partida(
     new Log
 );
 
-console.log(partida.jugadores)
 partida.agregarJugadoresTabla();
 partida.tabla.dibujarJugadoresEnLaTabla();
+
+
+// Botones de accion
+let btn_repartir = document.getElementById("repartir");
+let btn_truco = document.getElementById("truco");
+let btn_re_truco = document.getElementById("re-truco");
+let btn_vale_cuatro = document.getElementById("vale-cuatro");
+let btn_envido = document.getElementById("envido");
+let btn_real_envido = document.getElementById("real-envido");
+let btn_falta_envido = document.getElementById("falta-envido");
+let btn_flor = document.getElementById("flor");
+let btn_contra_flor = document.getElementById("contra-flor");
+let btn_flor_contra_al_resto = document.getElementById("flor-contra-al-resto");
+let btn_quiero = document.getElementById("quiero");
+let btn_no_quiero = document.getElementById("no-quiero");
+let btn_al_mazo = document.getElementById("al-mazo");
+
+btn_truco.addEventListener("click", () => {
+    partida.jugadores[0].truco();
+});
+
+btn_re_truco.addEventListener("click", () => {
+    partida.jugadores[0].reTruco();
+});
+
+btn_vale_cuatro.addEventListener("click", () => {
+    partida.jugadores[0].valeCuatro();
+});
+
+btn_envido.addEventListener("click", () => {
+    partida.jugadores[0].envido();
+});
+
+btn_real_envido.addEventListener("click", () => {
+    partida.jugadores[0].realEnvido();
+});
+
+btn_falta_envido.addEventListener("click", () => {
+    partida.jugadores[0].faltaEnvido();
+});
+
+btn_flor.addEventListener("click", () => {
+    partida.jugadores[0].flor();
+});
+
+btn_contra_flor.addEventListener("click", () => {
+    partida.jugadores[0].contraFlor();
+});
+
+btn_flor_contra_al_resto.addEventListener("click", () => {
+    partida.jugadores[0].contraFlorAlResto();
+});
+
+btn_quiero.addEventListener("click", () => {
+    partida.jugadores[0].aceptar_rechazar(true);
+});
+
+btn_quiero.addEventListener("click", () => {
+    partida.jugadores[0].aceptar_rechazar(false);
+});
+
+btn_no_quiero.addEventListener("click", () => {
+    partida.jugadores[0].rendirMano();
+});
 
 // function prueba() {
 //     let doc = document.getElementById("Mesa");
