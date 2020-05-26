738358df763177f2755f5e4ab8a8520c1d9cfcbe
panterx <panterx.95@gmail.com>
2020-05-25 21:16:48 -0300

    Rediseño de vista

--- js/animaciones.js
+++ js/animaciones.js
@@ -3,7 +3,7 @@ let cartas = document.getElementsByClassName("cartasEnMano");
 for(let i = 0; i < cartas.length; i++) {
     cartas[i].addEventListener("mouseover", () => {
         cartas[i].style.transform = "translateY(-10px)";
-        cartas[i].style.boxShadow = "10px 5px 5px #27ae60";
+        cartas[i].style.boxShadow = "5px 5px 5px #475577";
     })
     
     cartas[i].addEventListener("mouseout", () => {
