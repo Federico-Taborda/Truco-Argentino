f0fb42e4b023d68a49010eaf65fb713155e0da78
panterx <panterx.95@gmail.com>
2020-05-26 21:06:34 -0300

    Ordenando index

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
-const partida = new Partida(
-    [new Jugador("Fede", "Hombre"), new Jugador("CPU", "Mujer")],
-    new Mazo(),
-    30,
-    new Tabla()
-);
+const partida = new Partida([new Jugador("Fede", "Hombre"), new Jugador("CPU", "Mujer")], new Mazo(), 30, new Tabla());
 
 async function main() {
     partida.mazo.nuevoMazo();
     partida.agregarJugadoresTabla();
     partida.tabla.dibujarJugadoresEnLaTabla();
+    await partida.accion(partida.jugadores[1].tomarCartas(partida.mazo))
     await partida.accion(partida.jugadores[0].tomarCartas(partida.mazo))
     partida.mostrarCartas();
 }
