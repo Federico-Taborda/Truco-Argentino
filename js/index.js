c29b18e66947c2450fd185a987acf90063566b9e
panterx <panterx.95@gmail.com>
2020-11-03 18:29:05 -0300

    Agregando objeto de configuracion de partida

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-const partida = new Partida([new Humano("Fede", "Hombre"), new Cpu("CPU", "Mujer")], new Mazo(), 30, new Tabla());
+const config = {
+    puntos: 30
+};
+
+const partida = new Partida(
+    [new Humano("Fede", "Hombre"), new Cpu("CPU", "Mujer")],
+    new Mazo(), 
+    config.puntos, 
+    new Tabla());
 
 async function main() {
     partida.iniciarPartida();
