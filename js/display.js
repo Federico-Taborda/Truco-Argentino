0d56993f389c060ff81265019e12fc7eb8c39a0b
panterx <panterx.95@gmail.com>
2020-11-03 21:50:22 -0300

    Metodos de partida pasados a display y refactorizado

--- js/display.js
+++ js/display.js
@@ -2,4 +2,31 @@
 
 class Display {
 
+    // Muestra en pantalla las cartas del jugador
+    mostrarCartasEnMano(jugador) {
+        let manoJugador = jugador.mano;
+        document.getElementById("Jugador-Mano-1").src = manoJugador[0].url;
+        document.getElementById("Jugador-Mano-2").src = manoJugador[1].url;
+        document.getElementById("Jugador-Mano-3").src = manoJugador[2].url;
+    };
+
+    // Mueve a la mesa la carta jugada del jugador
+    mostrarCartaMesa(carta, jugador) {
+        if(jugador.cartasJugadas.length == 1) {
+            document.getElementById("Jugador-Mesa-1").src = carta.url;
+        }else if(jugador.cartasJugadas.length == 2) {
+            document.getElementById("Jugador-Mesa-2").src = carta.url;
+        }else if(jugador.cartasJugadas.length == 3) {
+            document.getElementById("Jugador-Mesa-3").src = carta.url;
+        };
+    };
+
+    // Muestra un elemento si display es true sino se oculta
+    displayElemento(id, display) {
+        if(display) {
+            document.getElementById(`${id}`).style.display = "initial";
+        }else{
+            document.getElementById(`${id}`).style.display = "none";
+        };
+    };
 }
--- js/partida.js
+++ js/partida.js
@@ -18,14 +18,14 @@ class Partida {
         await this.accion(this.reiniciarManoMesa());
         await this.jugadores.jugador.tomarCartas(this.mazo);
         await this.jugadores.cpu.tomarCartas(this.mazo);
-        await this.accion(this.mostrarCartasEnMano());
+        await this.accion(this.display.mostrarCartasEnMano(this.jugadores.jugador));
 
         if(this.jugadores.jugador.turnoActual) {
-            this.displayElemento("truco", true);
-            this.displayElemento("envido", true);
-            this.displayElemento("quiero", true);
-            this.displayElemento("no-quiero", true);
-            this.displayElemento("al-mazo", true);
+            this.display.displayElemento("truco", true);
+            this.display.displayElemento("envido", true);
+            this.display.displayElemento("quiero", true);
+            this.display.displayElemento("no-quiero", true);
+            this.display.displayElemento("al-mazo", true);
             this.esperarCartasJugador("Jugador-Mano-1", this.jugadores.jugador.mano[0]);
             this.esperarCartasJugador("Jugador-Mano-2", this.jugadores.jugador.mano[1]);
             this.esperarCartasJugador("Jugador-Mano-3", this.jugadores.jugador.mano[2]);
@@ -41,7 +41,7 @@ class Partida {
     juegaJugador(cantos, cartas) {
         // Muestra los cantos disponibles en el tablero
         for(let i = 0; i < cantos.length; i++) {
-            this.displayElemento(cantos[i], true);
+            this.display.displayElemento(cantos[i], true);
         };
         
         // Escucha los cantos del jugador
@@ -58,8 +58,8 @@ class Partida {
     esperarCartasJugador(id, carta) {
         document.getElementById(`${id}`).addEventListener("click", () => {
             this.jugadores.jugador.jugarCarta(carta);
-            this.displayElemento(id, false);
-            this.mostrarCartaMesa(carta);
+            this.display.displayElemento(id, false);
+            this.display.mostrarCartaMesa(carta, this.jugadores.jugador);
         });
     };
     
@@ -137,24 +137,7 @@ class Partida {
         return this.tabla.dibujarJugadoresEnLaTabla();
     };
 
-    // Muestra en pantalla las cartas del jugador
-    mostrarCartasEnMano() {
-        let manoJugador = this.jugadores.jugador.mano;
-        document.getElementById("Jugador-Mano-1").src = manoJugador[0].url;
-        document.getElementById("Jugador-Mano-2").src = manoJugador[1].url;
-        document.getElementById("Jugador-Mano-3").src = manoJugador[2].url;
-    };
-
-    // Mueve a la mesa la carta jugada del jugador
-    mostrarCartaMesa(carta) {
-        if(this.jugadores.jugador.cartasJugadas.length == 1) {
-            document.getElementById("Jugador-Mesa-1").src = carta.url;
-        }else if(this.jugadores.jugador.cartasJugadas.length == 2) {
-            document.getElementById("Jugador-Mesa-2").src = carta.url;
-        }else if(this.jugadores.jugador.cartasJugadas.length == 3) {
-            document.getElementById("Jugador-Mesa-3").src = carta.url;
-        };
-    };
+   
 
     compararCartasMesa(carta1, carta2) {
         if(carta1.valor > carta2.valor) {
@@ -176,15 +159,6 @@ class Partida {
             this.jugadores.jugador.turnoActual = true;
             this.jugadores.cpu.turnoActual = false;
             this.jugadores.jugador.generarLog(" Es mano");
-        };
-    };
-
-    // Si display es true se muestra sino se oculta
-    displayElemento(id, display) {
-        if(display) {
-            document.getElementById(`${id}`).style.display = "initial";
-        }else{
-            document.getElementById(`${id}`).style.display = "none";
         };
     };
 
